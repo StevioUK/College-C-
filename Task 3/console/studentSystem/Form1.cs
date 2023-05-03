b6ba2d3730c9803a826c9de542572bf4546f7e87
Stevio <59091187+StevioUK@users.noreply.github.com>
2023-05-03 19:50:35 +0100

    A BIG LOAD OF WORK
    Josh is daddy

---
+++ Task 3/console/studentSystem/Form1.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MySql.Data.MySqlClient;
+
+namespace studentSystem
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+
+    public class MySqlExecutor
+    {
+        private readonly string connectionString;
+
+        public MySqlExecutor(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        public DataTable ExecuteQuery(string query)
+        {
+            DataTable dataTable = new DataTable();
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    using (MySqlDataAdapter dataAdapter = new MySqlDataAdapter(command))
+                    {
+                        dataAdapter.Fill(dataTable);
+                    }
+                }
+            }
+
+            return dataTable;
+        }
+
+        public int ExecuteNonQuery(string query)
+        {
+            int affectedRows = 0;
+
+            using (MySqlConnection connection = new MySqlConnection(connectionString))
+            {
+                connection.Open();
+
+                using (MySqlCommand command = new MySqlCommand(query, connection))
+                {
+                    affectedRows = command.ExecuteNonQuery();
+                }
+            }
+
+            return affectedRows;
+        }
+    }
+}
---
+++ Task 3/console/studentSystem/Form2.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace studentSystem
+{
+    public partial class Form2 : Form
+    {
+        static string connectionString = "server=localhost;database=studentSystem;uid=user;password=test123;";
+        MySqlExecutor sqlExecutor = new MySqlExecutor(connectionString);
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            student stu = new student(textBox1.Text, textBox2.Text, textBox3.Text);
+            string insertQuery = String.Format("INSERT INTO students (name,dob,course) VALUES ({0}, {1}, {2});", stu.name, stu.dob,stu.course);//No " in the values field meaning I am passing through number values instead of strings, fix when Idont feel like crimzon and want to slice my wrists :)
+            int affectedRows = sqlExecutor.ExecuteNonQuery(insertQuery);
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+    class Person
+    {
+        public string name { get; }
+        public string dob { get; }
+
+        public Person(string name, string dob)
+        {
+            this.name = name;
+            this.dob = dob;
+        }
+    }
+
+    class student : Person
+    {
+        public string course { get; set; }
+        public student(string name, string dob, string course) : base(name, dob)
+        {
+            this.course = course;
+        }
+    }
+}
